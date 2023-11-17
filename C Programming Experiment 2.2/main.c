5e305f061bdf44f62796b0d50cfaacadbb8e15c0
Reyad07 <latif.tajnoor@gmail.com>
2023-11-17 14:43:51 +0800

    Experiment 2.2

---
+++ C Programming Experiment 2.2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int x, y, z;
+   // Input three integers //
+    printf("Enter three integers: ");
+    scanf("%d",&x);
+    scanf("%d",&y);
+    scanf("%d",&z);
+    // Sort the integers
+    int temp;
+    if (x > y) {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    if (x > z) {
+        temp = x;
+        x = z;
+        z = temp;
+    }
+    if (y > z) {
+        temp = y;
+        y = z;
+        z = temp;
+    }
+
+    // Output the sorted integers //
+    printf("Sorted integers: %d %d %d\n", x, y, z);
+
+    getch();
+}
